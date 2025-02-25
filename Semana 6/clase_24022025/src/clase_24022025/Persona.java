79cb134504390ecedb62fe6226ba6f2a149cfe3e
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2025-02-24 19:57:54 -0600

    clase 24 feb: tabbed pane ,JColorChooser

---
+++ Semana 6/clase_24022025/src/clase_24022025/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_24022025;
+
+/**
+ *
+ * @author claudiacortes
+ */
+public class Persona {
+    
+    public void Holamundo(){
+        
+    }
+    
+    public static void sininstanciar(){
+        
+    }
+    
+}
